424168cfe6e9afd7443d06b1384db4aedc717121
amokato <git@lowack.org>
2019-07-23 20:07:13 +0200

    Revert "add saturday"
    This reverts commit 9af5165939a8bfdd1d532749aadeab24773579cb.

--- Model/Carrier/Saturday.php
+++ Model/Carrier/Saturday.php
@@ -11,7 +11,7 @@
     use Magento\Shipping\Model\Carrier\CarrierInterface;
     use Magento\Shipping\Model\Rate\Result;
 
-    class Saturday extends AbstractCarrier implements CarrierInterface
+    class Priority extends AbstractCarrier implements CarrierInterface
     {
         /**
          * @var string
