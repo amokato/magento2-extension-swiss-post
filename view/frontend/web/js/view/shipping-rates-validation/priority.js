c22301927788f2970840c1b46a3ad7edcc16d2bc
Henrik Lowack <git@lowack.org>
2019-07-28 16:04:19 +0200

    fallback to default

--- view/frontend/web/js/view/shipping-rates-validation/priority.js
+++ view/frontend/web/js/view/shipping-rates-validation/priority.js
@@ -9,8 +9,8 @@ define(
         'uiComponent',
         'Magento_Checkout/js/model/shipping-rates-validator',
         'Magento_Checkout/js/model/shipping-rates-validation-rules',
-        '../../model/shipping-rates-validator/priority',
-        '../../model/shipping-rates-validation-rules/priority'
+        '../../model/shipping-rates-validator/default',
+        '../../model/shipping-rates-validation-rules/default'
     ],
     function (
         Component,
